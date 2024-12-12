3708184ed1ad42cd2e49aa4edc7fff7e344ab2a0
kwangyoulsagong <sgky0511@naver.com>
2024-12-12 19:19:17 +0900

    feat: cors allow

--- backend/server.js
+++ backend/server.js
@@ -19,11 +19,12 @@ app.set("trust proxy", 1);
 // CORS 설정
 const corsOptions = {
   origin: [
-    "https://bootcampchat-fe.run.goorm.site",
-    "http://localhost:3000",
-    "https://localhost:3000",
-    "http://0.0.0.0:3000",
-    "https://0.0.0.0:3000",
+    "https://chat.goorm-ktb-009.goorm.team",
+    "http://chat.goorm-ktb-009.goorm.team",
+    "https://chat.goorm-ktb-009.goorm.team/5000",
+    "http://chat.goorm-ktb-009.goorm.team/5000",
+    "https://chat.goorm-ktb-009.goorm.team/5001",
+    "http://chat.goorm-ktb-009.goorm.team/5001",
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
